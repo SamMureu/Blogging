cbc56a6f56058389f6b74eeceb0f87c285736699
SamMureu <samkimari@gmail.com>
2024-04-29 19:43:54 +0300

    Initial commit

--- resources/views/home/service.blade.php
+++ resources/views/home/service.blade.php
@@ -1,7 +1,7 @@
 <div class="services_section layout_padding">
          <div class="container">
             <h1 class="services_taital">Services </h1>
-            <p class="services_text">🌟 Welcome to Our Blogging Community! 🌟
+            <p class="services_text">🌟 Welcome to Our Blogging Community! 🌟<br>
 
                At MUR TECH, we're dedicated to providing you with a captivating and enriching blogging experience. Here's how we make your journey with us truly remarkable:
                
